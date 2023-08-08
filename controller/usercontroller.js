cf790ccf270cf935836f6b5e457421f8b621dcba
harman <harmandeep.expinator@gmail.com>
2023-08-08 15:27:33 +0530

    change in get urldata api

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -49,63 +49,97 @@ exports.deletedata = async (req, res) => {
         res.send({ status: false, message: "Something went wrong!!" })
     }
 }
+// exports.geturldata = async (req, res) => {
+//     try {
+
+//         const response = await axios.get("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", {
+//             headers: {
+//                 'X-CMC_PRO_API_KEY': 'f2128a7b-ead4-449b-834d-b460ea734d73',
+//             },
+//             // params: {
+//             //     limit: 10, // Number of cryptocurrencies to retrieve
+//             //     convert: 'USD', // Convert prices to USD
+//             // },
+//         });
+//         const data = response.data.data;
+//         let tvl = 0;
+//         const formattedData = data.map((coin) => ({
+//             if(tvl = 0) {
+//                 const tvlResponse = axios.get(`https://api.defipulse.com/api/v1/defipulse/GetHistory?period=1d&project=${coin.slug}`);
+//                 const tvlData = tvlResponse.data;
+//                 tvl = tvlData[0]?.tvl || 0;
+//             },
+//             name: coin.name,
+//             price: `${coin.quote.USD.price}`,
+//             priceChange: `${coin.quote.USD.percent_change_24h}%`,
+//             volume: `${coin.quote.USD.volume_24h}`,
+//             tvl: tvl,
+//         }));
+
+//         res.send({ status: true, message: "Successfully get data", details: formattedData })
+//     } catch (error) {
+//         console.log('Error:', error);
+//         return null;
+//     }
+
+// }
 exports.geturldata = async (req, res) => {
     try {
+        req.query.param = "62d7ee6tev8"
 
-        const response = await axios.get("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", {
+        const { slug, start, limit, category, centerType, sort, direction } = req.query
+        const response = await axios.get(`https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=${slug}&start=${start}&limit=${limit}&category=${category}&centerType=${centerType}&sort=${sort}&direction=${direction}`, {
             headers: {
-                'X-CMC_PRO_API_KEY': process.env.API_KEY_coinmarketcap,
+                'X-CMC_PRO_API_KEY': 'f2128a7b-ead4-449b-834d-b460ea734d73',
             },
-            // params: {
-            //     limit: 10, // Number of cryptocurrencies to retrieve
-            //     convert: 'USD', // Convert prices to USD
-            // },
         });
-        const data = response.data.data;
-        let tvl = 0;
+        let data = response.data.data.marketPairs
         const formattedData = data.map((coin) => ({
-            if(tvl = 0) {
-                const tvlResponse = axios.get(`https://api.defipulse.com/api/v1/defipulse/GetHistory?period=1d&project=${coin.slug}`);
-                const tvlData = tvlResponse.data;
-                tvl = tvlData[0]?.tvl || 0;
-            },
-            name: coin.name,
-            price: `${coin.quote.USD.price}`,
-            priceChange: `${coin.quote.USD.percent_change_24h}%`,
-            volume: `${coin.quote.USD.volume_24h}`,
-            tvl: tvl,
+
+            name: coin.exchangeName,
+            marketPair: coin.marketPair,
+            price: Number(coin.price).toFixed(8),
+            volume: Math.round(coin.volumeUsd).toFixed(2),
+            liquidity_score: Math.round(coin.effectiveLiquidity),
+            lastupdate: coin.lastUpdated
+
+            // price: `${coin.quote.USD.price}`,
+            // market_pair: coin.market_pair,
+            // volume: `${coin.quote.USD.volume_24h}`,
+            // update: `${coin.quote.USD.last_updated}`,
+
+            // priceChange: `${coin.quote.USD.percent_change_24h}%`,
+            // volume: `${coin.quote.USD.volume_24h}`,
         }));
 
         res.send({ status: true, message: "Successfully get data", details: formattedData })
     } catch (error) {
-        console.log('Error:', error);
-        return null;
+        console.log(error);
     }
+},
 
-}
+    exports.updatedata = async (req, res) => {
+        try {
+            const { mainHeading, secondHeading, innerText } = req.body
 
-exports.updatedata = async (req, res) => {
-    try {
-        const { mainHeading, secondHeading, innerText } = req.body
+            const findByid = await usermodel.findById(req.params.id)
 
-        const findByid = await usermodel.findById(req.params.id)
+            const protocol = req.protocol;
+            const host = req.hostname;
+            const fullUrl = process.env.URL
+            console.log("#######3", process.env.URL)
+            let update = await usermodel.findByIdAndUpdate(req.params.id, {
+                mainHeading: mainHeading,
+                secondHeading: secondHeading,
+                innerText: innerText,
+                image: req.file ? req.file.filename : "",
+            }, { new: true })
 
-        const protocol = req.protocol;
-        const host = req.hostname;
-        const fullUrl = process.env.URL
-        console.log("#######3", process.env.URL)
-        let update = await usermodel.findByIdAndUpdate(req.params.id, {
-            mainHeading: mainHeading,
-            secondHeading: secondHeading,
-            innerText: innerText,
-            image: req.file ? req.file.filename : "",
-        }, { new: true })
+            await update.save()
 
-        await update.save()
+            res.send({ status: true, message: "Successfully update data", data: update })
 
-        res.send({ status: true, message: "Successfully update data", data: update })
-
-    } catch (error) {
-        res.send({ status: false, message: error.message })
+        } catch (error) {
+            res.send({ status: false, message: error.message })
+        }
     }
-}
