82f7109d112d82d24d7328c22ffdea3ece210038
harman <harmandeep.expinator@gmail.com>
2023-08-09 18:09:29 +0530

    change in get data api

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -49,30 +49,61 @@ exports.deletedata = async (req, res) => {
 }
 exports.geturldata = async (req, res) => {
     try {
-        req.query.param = "62d7ee6tev8"
 
-        const { slug, start, limit, category, centerType, sort, direction } = req.query
-        const response = await axios.get(`https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=${slug}&start=${start}&limit=${limit}&category=${category}&centerType=${centerType}&sort=${sort}&direction=${direction}`, {
+        //const gasPriceBNB = 500000000 / 1e18;
+
+        // Step 3: Fetch the BNB to USD conversion rate from a data provider or exchange API
+        const bnbToUsdRate = 300; // Example rate, should be fetched from a reliable source
+
+        // Step 4: Calculate the gas price in USD
+        //const gasPriceUSD = gasPriceBNB * bnbToUsdRate;
+
+        const response = await axios.get(`https://api.bscscan.com/api?module=account&action=txlist&address=0x9E24415d1e549EBc626a13a482Bb117a2B43e9CF&sort=asc`, {
             headers: {
-                'X-CMC_PRO_API_KEY': 'f2128a7b-ead4-449b-834d-b460ea734d73',
+                'apikey': 'K2XHBWECVI7MD74WJQEUTMM431CDFM5F86',
             },
         });
-        let data = response.data.data.marketPairs
-        const formattedData = data.map((coin) => ({
+        let data = response.data.result
+        const formattedData = data.map((coin) => {
+            let details = {
+                TxnHash: coin.hash,
+                price: Number(coin.gasPrice/1e18 * bnbToUsdRate).toFixed(7),
+                value: Number(coin.value/1e18 * bnbToUsdRate).toFixed(8)
+            }
+            return details;
+        })
 
-            name: coin.exchangeName,
-            marketPair: coin.marketPair,
-            price: Number(coin.price).toFixed(8),
-            volume: Math.round(coin.volumeUsd).toFixed(2),
-            liquidity_score: Math.round(coin.effectiveLiquidity),
-            lastupdate: coin.lastUpdated
-        }));
-
-        res.send({ status: true, message: "Successfully get data", details: formattedData })
+        res.send({ status: true, message: "Successfully get data", formattedData })
     } catch (error) {
         console.log(error);
     }
 },
+    // exports.geturldata = async (req, res) => {
+    //     try {
+    //         req.query.param = "62d7ee6tev8"
+
+    //         const { slug, start, limit, category, centerType, sort, direction } = req.query
+    //         const response = await axios.get(`https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=${slug}&start=${start}&limit=${limit}&category=${category}&centerType=${centerType}&sort=${sort}&direction=${direction}`, {
+    //             headers: {
+    //                 'X-CMC_PRO_API_KEY': 'f2128a7b-ead4-449b-834d-b460ea734d73',
+    //             },
+    //         });
+    //         let data = response.data.data.marketPairs
+    //         const formattedData = data.map((coin) => ({
+
+    //             name: coin.exchangeName,
+    //             marketPair: coin.marketPair,
+    //             price: Number(coin.price).toFixed(8),
+    //             volume: Math.round(coin.volumeUsd).toFixed(2),
+    //             liquidity_score: Math.round(coin.effectiveLiquidity),
+    //             lastupdate: coin.lastUpdated
+    //         }));
+
+    //         res.send({ status: true, message: "Successfully get data", details: formattedData })
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // },
 
     exports.updatedata = async (req, res) => {
         try {
